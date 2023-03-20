affa853b3d011e84db5647d908437be94324a122
Moonshie <moonshie.ua@gmail.com>
2023-03-20 10:19:25 +0300

    implement placeholder usability

--- project-library/script.js
+++ project-library/script.js
@@ -12,3 +12,14 @@ class Book {
 const book1 = new Book('Brandon Sanderson', 'The Final Empire', 541, true)
 const book2 = new Book('Jim Butcher', 'Storm Front', 322, true)
 myLibrary.push(book1, book2)
+
+const NEW_BOOK_BUTTON = document.getElementById('new-book')
+const GALLERY = document.querySelector('.gallery')
+
+function addBook() {
+  const CARD = document.createElement('div')
+  CARD.className = 'book-card'
+  GALLERY.appendChild(CARD)
+}
+
+NEW_BOOK_BUTTON.addEventListener('click', addBook)
